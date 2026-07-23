e87feaa67ec9b54e70e6831777ce280850a71455
hiz <kuznecovmatvej717@gmail.com>
2026-07-23 14:37:36 +0500

    Добавил карточку hiz.log

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // Указываем имя репозитория, чтобы GitHub Pages правильно находил JS, CSS и картинки
-  base: '/docs',
+  base: '/',
 
   build: {
     // Папка, куда Vite соберет готовый продакшн-сайт
